6b211eca247661c37af84f6c497cb3173e721e74
GuanglongDu <duguanglong002@163.com>
2017-05-25 22:06:05 +0800

    Change AppComponent6

--- app/upgrade/app.component.ts
+++ app/upgrade/app.component.ts
@@ -23,7 +23,7 @@ import { UpgradeModule } from '@angular/upgrade/static';
   selector: 'app-root',
   template: `<div ng-view class="view-frame"></div>`
 })
-export class AppComponent5 implements OnInit {
+export class AppComponent6 implements OnInit {
 
   constructor(private upgrade: UpgradeModule) {}
 
